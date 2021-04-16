4e0f510d5cf592c0ea6864a1988a55c4c53b0938
Matthieu <matthieu.mauny@gmail.com>
2021-04-16 15:58:29 +0200

    dispatch fetchAsync action

--- src/features/message/messageList.js
+++ src/features/message/messageList.js
@@ -1,20 +1,19 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { fetchAll } from "./messageAPI";
-import { selectMessage } from "./messageSlice";
+import { fetchAsync, selectMessage } from "./messageSlice";
 
 export function MessageList() {
   const messages = useSelector(selectMessage);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(fetchAll);
+    dispatch(fetchAsync());
   }, [dispatch]);
 
   return (
     <div>
       {messages.map((message) => (
-        <span>{message.description}</span>
+        <span>{message.content}</span>
       ))}
     </div>
   );
